baef6b3ac677a7fa412d0bec3d03f8a060e3aeeb
Tomoki826 <uda.tomoki@icloud.com>
2024-10-23 22:46:28 +0900

    機能追加

--- fe/src/components/atoms/singleCard/index.tsx
+++ fe/src/components/atoms/singleCard/index.tsx
@@ -1,26 +1,53 @@
-import React from 'react'
-import styles from './index.module.scss'
+import React, { useState } from 'react'
+import styles from './index.module.css'
 
 type SingleCardProperties = {
-  color: 'red' | 'blue' | 'green' | 'yellow' | 'purple'
+  color: string //'red' | 'blue' | 'green' | 'yellow' | 'purple'
   message: string
+  cardIndex: number
+  cardState: number
+  handleClick: (cardNumber: number) => void
 }
 
 export const SingleCard: React.FC<SingleCardProperties> = ({
   color,
   message,
+  cardIndex,
+  cardState,
+  handleClick,
 }) => {
+  // 色を変換
   let bgRGB = '#eeeeee'
   if (color === 'red') bgRGB = '#e2767c'
   if (color === 'blue') bgRGB = '#5bb3e7'
   if (color === 'green') bgRGB = '#a4cb4c'
   if (color === 'yellow') bgRGB = '#fdd15b'
   if (color === 'purple') bgRGB = '#b885cd'
+
   return (
     <>
-      <div className={styles.container} style={{ backgroundColor: bgRGB }}>
-        <div className={styles.cardMessage}>
-          {message}
+      <div className={styles.container}>
+        {cardState > 0 ? (
+          <div
+            style={{
+              position: 'absolute',
+              zIndex: 10000,
+              top: '8%',
+              right: '5%',
+            }}
+          >
+            <img
+              src={`images/icon/${cardState}.png`}
+              style={{ width: '50px' }}
+            />
+          </div>
+        ) : null}
+        <div
+          className={styles.box}
+          style={{ backgroundColor: bgRGB, opacity: cardState > 0 ? 0.5 : 1 }}
+          onClick={() => handleClick(cardIndex)}
+        >
+          <div className={styles.cardMessage}>{message}</div>
         </div>
       </div>
     </>
--- fe/src/components/pages/card/index.tsx
+++ fe/src/components/pages/card/index.tsx
@@ -1,25 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SingleCard } from '../../atoms'
 import styles from './index.module.scss'
 
 export const Card: React.FC = () => {
+  // お題
+  const question = 'エンジニアとして喜ぶ瞬間'
+
+  // カード内容
+  const cardData = [
+    { color: 'red', message: 'プログラムが正常に動いた時' },
+    { color: 'blue', message: '課題チェックが上手く進んだ時' },
+    { color: 'green', message: 'バグの原因が分かった時' },
+    { color: 'yellow', message: '基本情報技術者試験で合格した時' },
+    { color: 'purple', message: '仕事を済ませて早く退勤できた時' },
+  ]
+
+  // カードの状態 (0は押されていない、1から5は押した順番)
+  const [cardsState, setCardsState] = useState([0, 0, 0, 0, 0])
+
+  // 発火イベント
+  const resetState = () => {
+    setCardsState([0, 0, 0, 0, 0])
+  }
+
+  const handleClick = (cardNumber: number) => {
+    console.log(cardsState)
+
+    if (cardsState[cardNumber] !== 0) return
+
+    const maxValue = Math.max(...cardsState)
+
+    // 番号をつける
+    const updateCardsState = cardsState.map((item, index) =>
+      cardNumber === index ? maxValue + 1 : item,
+    )
+    setCardsState(updateCardsState)
+  }
+
   return (
     <>
       <div className={styles.container}>
         <div className={styles.question}>
           <div className={styles.questionBox}>
             <div className={styles.questionTitle}>お題</div>
-            <div className={styles.questionMessage}>
-              エンジニアとして喜ぶ瞬間
-            </div>
+            <div className={styles.questionMessage}>{question}</div>
           </div>
         </div>
-        <div className={styles.cards}>
-          <SingleCard color="red" message="プログラムが正常に動いた時" />
-          <SingleCard color="blue" message="課題チェックが上手く進んだ時" />
-          <SingleCard color="green" message="バグの原因が分かった時" />
-          <SingleCard color="yellow" message="基本情報技術者試験で合格した時" />
-          <SingleCard color="purple" message="仕事を済ませて早く退勤できた時" />
+        <div className={styles.rowItems}>
+          <div className={styles.cards}>
+            {cardData.map((singleCard, index) => (
+              <SingleCard
+                color={singleCard.color}
+                message={singleCard.message}
+                cardIndex={index}
+                cardState={cardsState[index]}
+                handleClick={handleClick}
+              />
+            ))}
+          </div>
+          <div className={styles.buttonColumn}>
+            <img
+              src="/images/team.png"
+              className={styles.teamMember}
+              onClick={undefined}
+            />
+            <img
+              src="/images/confirmation.png"
+              className={styles.confirmation}
+              onClick={undefined}
+            />
+            <img
+              src="/images/trashButton.png"
+              className={styles.resetImage}
+              onClick={resetState}
+            />
+          </div>
+        </div>
+        <div className={styles.modalContainer}>
+          <div className={styles.modalBox}>
+            <div className={styles.modalIcon}>
+              <img src="" className={styles.iconImage} alt="icon" />
+            </div>
+            ああああ
+          </div>
         </div>
       </div>
     </>
